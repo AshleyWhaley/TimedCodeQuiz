ef2d6112b1eead8d5582caaabc5b1d80c03cb152
Ashley Whaley <oms_t@hotmail.com>
2021-01-13 23:00:34 -0800

    Added first Question and centered start button

---
+++ script.js
@@ -0,0 +1,60 @@
+/*1. What does HTML stand for?
+Hypertext Machine Language 
+Hightech Madeup Language 
+Hypertext Markup Language **
+Hyper Technical Markup Language 
+
+2. Which of the following attributes is used to add link to any element?
+link
+ref
+href **
+click
+
+3. How can you open a link in a new browser window?  
+A href="url" target="_blank" **
+A href="url" new
+A href="url" target="new"
+A href="url" target=_window"
+
+4. What is the name of every homepage on the WWW?
+Home.html
+ Index.html **
+Anything you want it to be.html
+The name of your website.html
+
+5. How do you insert a comment in a CSS file? 
+' this is a comment
+/* this is a comment */ **
+// this is a comment
+// this is a comment //
+
+/*6. Where in an HTML document is the correct place to refer to an external style sheet?
+In the head section **
+At the end of the document
+At the top of the document
+In the body section
+
+7. What is the correct way to write a JavaScript array?
+var colors = "red", "green", "blue"
+var colors = (1:"red", 2:"green", 3:"blue")
+var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")
+var colors = ["red", "green", "blue"]  **
+
+8. How does a FOR loop start?
+for (i = 0; i <= 5)
+for i = 1 to 5
+for (i = 0; i <= 5; i++) **
+for (i <= 5; i++)
+
+9. How to write an IF statement for executing some code if "i" is NOT equal to 5?
+if (i != 5)  **
+if i <> 5
+if (i <> 5)
+if i =! 5 then
+
+10. How do you call a function named "myFunction"?
+call myFunction()
+run myFunction()
+call function myFunction()
+myFunction()  **
+*/
