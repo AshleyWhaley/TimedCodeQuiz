13c3b5529b909203cd74e99b94fcc4e738154d59
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-01-18 22:50:32 -0800

    centered text

--- script.js
+++ script.js
@@ -14,8 +14,118 @@ var form = document.querySelector(".submitForm");
 var index = 0;
 var score = 0;
 
+// Setting an array for the question variable to include questions, answers and correct answers 
+var questionList = [
+    {
+      question: "1. What does HTML stand for?",
+      answers: [
+        "Hypertext Machine Language",
+        "Hightech Madeup Language",
+        "Hypertext Markup Language", 
+        "Hyper Technical Markup Language"
+      ],
+      rightAnswer: "Hypertext Markup Language"
+    },
+    {
+      question: "2. Which of the following attributes is used to add link to any element?",
+      answers: [
+        "link",
+        "ref",
+        "href", 
+        "click"
+      ],
+      rightAnswer: "href"
+    },
+    {
+      question: "3. How can you open a link in a new browser window?",
+      answers: [
+        "A href='url' target='_blank'",
+        "A href='url' new",
+        "A href='url' target='new'",
+        "A href='url' target='_window'"
+      ], 
+      rightAnswer: "A href='url' target='_blank'"
+    },
+    {
+      question:
+        "4. What is the name of every homepage on the WWW?",
+        answers: [
+          "home.html",
+          "index.html",
+         "Anything you want it to be.html",
+         "The name of your website.html"
+        ], 
+      rightAnswer: "index.html"
+    },
+    {
+      question:
+        "5. What is a useful tool used during development and debugging for printing content to the debugger?",
+      answers: [
+        "for loops",
+        "The terminal",
+        "console.log",
+        "Download an extension"
+      ],
+      rightAnswer: "for loops"
+    },
+    { 
+      question:
+      "6. Where in an HTML document is the correct place to refer to an external style sheet?",
+      answers: [
+          "In the head section", 
+          "At the end of the document",
+          "At the top of the document",
+          "In the body section"
+      ],
+      rightAnswer: "In the head section"
+  },
+  { 
+      question:
+      "7. What is the correct way to write a JavaScript array?",
+      answers: [
+          "var colors = 'red', 'green', 'blue'",
+          "var colors = (1:'red', 2:'green', 3:'blue')",
+          "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+          "var colors = ['red', 'green', 'blue']"
+      ],
+      rightAnswer: "var colors = ['red', 'green', 'blue']"
+  },
+  { 
+      question:
+      "8. How does a FOR loop start?",
+      answers: [
+          "for (i = 0; i <= 5)",
+          "for i = 1 to 5",
+          "for (i = 0; i <= 5; i++)", 
+          "for (i <= 5; i++)"
+      ],
+      rightAnswer: "for (i = 0; i <= 5; i++)"
+  },
+  { 
+      question:
+      "9. How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+      answers: [
+          "if (i != 5)",  
+          "if i <> 5",
+          "if (i <> 5)",
+          "if i =! 5 then"
+      ],
+      rightAnswer: "if (i != 5)"
+  },
+  { 
+      question:
+      "10. How do you call a function named 'myFunction'?",
+      answers: [
+          "call myFunction();",
+          "run myFunction();",
+          "call function myFunction();",
+          "myFunction();"  
+      ],
+      rightAnswer: "myFunction();"
+  },
+  ];
 
-// Using addEventListener on "Start Quiz" button to start the timer:
+// Event Listener to "Start Quiz" timer:
 function startQuiz() {
     start.addEventListener("click", function(event) {
       if (remainingSeconds < 60) {
@@ -29,7 +139,7 @@ function startQuiz() {
   }
 
 
-// Use function timer() to countdown from remainingSeconds:
+// Timer function to countdown from remainingSeconds:
 function timer() {
     var timerInterval = setInterval(function() {
       remainingSeconds--;
@@ -41,121 +151,10 @@ function timer() {
     }, 1000);
   }
 
-// Set an array as a variable that includes questions, answers, and correct answers:
-var questionList = [
-  {
-    question: "1. What does HTML stand for?",
-    answers: [
-      "Hypertext Machine Language",
-      "Hightech Madeup Language",
-      "Hypertext Markup Language", 
-      "Hyper Technical Markup Language"
-    ],
-    rightAnswer: "Hypertext Markup Language"
-  },
-  {
-    question: "2. Which of the following attributes is used to add link to any element?",
-    answers: [
-      "link",
-      "ref",
-      "href", 
-      "click"
-    ],
-    rightAnswer: "href"
-  },
-  {
-    question: "3. How can you open a link in a new browser window?",
-    answers: [
-      "A href='url' target='_blank'",
-      "A href='url' new",
-      "A href='url' target='new'",
-      "A href='url' target='_window'"
-    ], 
-    rightAnswer: "A href='url' target='_blank'"
-  },
-  {
-    question:
-      "4. What is the name of every homepage on the WWW?",
-      answers: [
-        "home.html",
-        "index.html",
-       "Anything you want it to be.html",
-       "The name of your website.html"
-      ], 
-    rightAnswer: "index.html"
-  },
-  {
-    question:
-      "5. What is a useful tool used during development and debugging for printing content to the debugger?",
-    answers: [
-      "for loops",
-      "The terminal",
-      "console.log",
-      "Download an extension"
-    ],
-    rightAnswer: "for loops"
-  },
-  { 
-    question:
-    "6. Where in an HTML document is the correct place to refer to an external style sheet?",
-    answers: [
-        "In the head section", 
-        "At the end of the document",
-        "At the top of the document",
-        "In the body section"
-    ],
-    rightAnswer: "In the head section"
-},
-{ 
-    question:
-    "7. What is the correct way to write a JavaScript array?",
-    answers: [
-        "var colors = 'red', 'green', 'blue'",
-        "var colors = (1:'red', 2:'green', 3:'blue')",
-        "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
-        "var colors = ['red', 'green', 'blue']"
-    ],
-    rightAnswer: "var colors = ['red', 'green', 'blue']"
-},
-{ 
-    question:
-    "8. How does a FOR loop start?",
-    answers: [
-        "for (i = 0; i <= 5)",
-        "for i = 1 to 5",
-        "for (i = 0; i <= 5; i++)", 
-        "for (i <= 5; i++)"
-    ],
-    rightAnswer: "for (i = 0; i <= 5; i++)"
-},
-{ 
-    question:
-    "9. How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-    answers: [
-        "if (i != 5)",  
-        "if i <> 5",
-        "if (i <> 5)",
-        "if i =! 5 then"
-    ],
-    rightAnswer: "if (i != 5)"
-},
-{ 
-    question:
-    "10. How do you call a function named 'myFunction'?",
-    answers: [
-        "call myFunction();",
-        "run myFunction();",
-        "call function myFunction();",
-        "myFunction();"  
-    ],
-    rightAnswer: "myFunction();"
-},
-];
-
 // In order for the user to be prompted with a questions and for the questions to be in a "new screen"
 function insertQuestion() {
   container.innerHTML = "<h2>" + questionList[index].question + "</h2>";
-  container.style.textAlign = "left";
+  container.style.textAlign = "center";
   for (var i = 0; i < questionList[index].answers.length; i++) {
     document.querySelector(".answer" + i).innerHTML =
       "<button type='button' class='btn btn-primary'>" +
